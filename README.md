f2b641b19ddf9715b2d5b67739334a471f05b513
Bob Budd <bob.budd@gmail.com>
2015-01-29 16:58:17 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # speechticulate
 webkit speech recognition for articulate presentations
+
+to install:
+- get this repo as well as the `voicecontrol` branch of `dark-spider`.
+- open your `chrome://extensions` page and make sure "Developer mode" checkbox is checked in the upper right corner.
+- click the "Load unpacked extension" button.
+- navigate to this folder on your local machine, and select the `v0.1` folder.
+
+currently working for "next" command and in localhost only (trying to figure out how to allow chrome extensions in the file protocol).
+
---
+++ v0.1/scripts/app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var slice = Array.prototype.slice;
+
+var app = {
+
+  config : {
+    next: 'sendClick',
+    back: 'sendClick'
+  },
+
+  methods: {
+    sendClick: function(transcript) {
+      var button = document.querySelector('[data-voice="' + transcript + '"]');
+      if (button) {
+        console.log(transcript);
+        button.dispatchEvent(new MouseEvent("click"));
+        app.flashRecognized();
+      }
+    }
+  },
+
+  onResult : function(evt) {
+    var result, transcript,
+        results = slice.call(evt.results, evt.resultIndex);
+
+    results.forEach(function(result) {
+      if (!result.isFinal) { return; }
+
+      var transcript = result[0].transcript.trim().toLowerCase(),
+          method = app.config[transcript];
+
+      if (method != null) {
+        app.methods[method](transcript);
+      }
+
+    });
+  },
+
+  flashRecognized: function() {
+    document.body.style.backgroundColor = 'red';
+    window.requestAnimationFrame(function() {
+      document.body.style.transition = "background-color 0.5s ease";
+      document.body.style.backgroundColor = '';
+    });
+  }
+
+}
+
+var Voice = function() {
+  this.recognition = new webkitSpeechRecognition();
+  this.recognition.continuous = true;
+  this.recognition.interimResults = false;
+  this.recognition.lang = 'en-US';
+  this.listeners = {};
+}
+
+var voiceProto = {
+
+  addListener : function(evt, callback) {
+    this.listeners[evt] = this.listeners[evt] || [];
+    this.listeners[evt].push(callback);
+    this.recognition['on' + evt] = callback;
+    return this;
+  },
+
+  removeListener : function(evt, callback) {
+    var indexOf;
+    if (this.listeners[evt]) {
+      if (!callback) {
+        this.listeners[evt].forEach(function(cb) {
+          this.recognition.removeEventListener(evt, cb);
+        });
+        this.listeners[evt] = null;
+      } else if (callback && (indexOf = this.listeners[evt].indexOf(callback))) {
+        this.listeners[evt].splice(indexOf, 1);
+        this.recognition.removeEventListener(evt, callback);
+      }
+    }
+    return this;
+  },
+
+  start : function() {
+    this.recognition.start();
+    return this;
+  },
+
+  stop : function() {
+    this.recognition.stop();
+    return this;
+  }
+
+}
+
+Voice.prototype = voiceProto;
+
+app.voice = new Voice();
+
+app.voice.start().addListener('result', app.onResult);
